135deecb932ed01000a6d1e97bafbcce7f994aab
SteveDowning <downingindeed@gmail.com>
2018-01-01 14:11:11 -0500

    Initial Solution Setup

---
+++ StevesShop/StevesShop.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StevesShop.WebUI.MvcApplication" Language="C#" %>
---
+++ StevesShop/StevesShop.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StevesShop.WebUI.Startup))]
+namespace StevesShop.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
