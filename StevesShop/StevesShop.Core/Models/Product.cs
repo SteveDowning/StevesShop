885374063825cea17fa6743aa875b456d013fcb9
SteveDowning <downingindeed@gmail.com>
2018-03-04 13:00:05 -0500

    Added In Memory Repository

--- StevesShop/StevesShop.Core/Models/Product.cs
+++ StevesShop/StevesShop.Core/Models/Product.cs
@@ -8,9 +8,8 @@ using System.Threading.Tasks;
 
 namespace StevesShop.Core.Models
 {
-    public class Product
+    public class Product : BaseEntity
     {
-        public string Id { get; set; }
 
         [StringLength(20)]
         [DisplayName("Product Name")]
@@ -20,11 +19,6 @@ namespace StevesShop.Core.Models
         [Range(0, 1000)]
         public decimal Price { get; set; }
         public string Category { get; set; }
-        public string Image { get; set; }
-
-        public Product()
-        {
-            this.Id = Guid.NewGuid().ToString();
-        }
+        public string Image { get; set; }       
     }
 }
